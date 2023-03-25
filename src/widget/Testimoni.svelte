3cd2e3ab45f9ce196c51ceb161f67d344358a104
agungbharata <dripsendermu@gmail.com>
2023-03-25 07:46:58 +0700

    revisi 1

---
+++ src/widget/Testimoni.svelte
@@ -0,0 +1,18 @@
+<script>
+  export let src = 'https://dripsender.id/wp-content/uploads/2023/02/testi1.jpg'
+  import { scale } from 'svelte/transition'
+  import { onMount } from 'svelte'
+  export let alt
+
+  let visible = false
+
+  onMount(() => {
+    visible = true
+  })
+</script>
+
+{#if visible}
+  <img class="h-auto max-w-full" {alt} transition:scale={{ delay: 1000, duration: 1000 }} {src} />
+{:else}
+  <div class="invisible  h-48  ">.</div>
+{/if}
